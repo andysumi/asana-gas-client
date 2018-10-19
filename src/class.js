ca23dd7d31b29c00de065dc6116332372ea30a20
andysumi <andysumi@ruby.net.tokai-u.jp>
2018-10-19 17:42:31 +0900

    getProjectsInWorkspaceメソッドを追加

--- src/class.js
+++ src/class.js
@@ -7,6 +7,24 @@
       this.projectId = projectId;
       if (!token) throw new Error('"token"は必須です');
     }
+
+    AsanaClient.prototype.getProjectsInWorkspace = function (workspaceId) {
+      var id = workspaceId || this.workspaceId;
+      return this.fetch_(Utilities.formatString('/workspaces/%d/projects', id), { 'method': 'get' });
+    };
+
+    AsanaClient.prototype.fetch_ = function (endPoint, options) {
+      var url = this.apiUrl + endPoint;
+      var response = UrlFetchApp.fetch(url, {
+        'method': options.method,
+        'muteHttpExceptions': true,
+        'contentType': 'application/json; charset=utf-8',
+        'headers': this.headers,
+        'payload': options.payload || {}
+      });
+      return JSON.parse(response.getContentText());
+    };
+
     return AsanaClient;
   })();
   return global.AsanaClient = AsanaClient;
--- src/main.js
+++ src/main.js
@@ -8,3 +8,12 @@
 function create(token, workspaceId, projectId) { // eslint-disable-line no-unused-vars
   return new AsanaClient(token, workspaceId, projectId);
 }
+
+/**
+ * Workspace内のProjectを取得する
+ * @param {Integer} workspaceId 【任意】ワークスペースID
+ * @return {Object} Projectのオブジェクト
+ */
+function getProjectsInWorkspace(workspaceId) { // eslint-disable-line no-unused-vars
+  throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
+}
