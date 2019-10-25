c97e4609f14f482d094d77e65dfd715a193fbf15
andysumi <andysumi@ruby.net.tokai-u.jp>
2019-10-25 17:08:35 +0900

    countProjectTasksメソッドを追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /**
  * Asana Clientのインスタンスを作成する
- * @param {String} token 【必須】アクセストークン
- * @param {Integer} workspaceId 【任意】ワークスペースID
- * @param {Integer} projectId 【任意】プロジェクトID
+ * @param {string} token 【必須】アクセストークン
+ * @param {?number} workspaceId 【任意】ワークスペースID
+ * @param {?number} projectId 【任意】プロジェクトID
  * @return {AsanaClient} Asana Clientのインスタンス
  */
 function create(token, workspaceId, projectId) { // eslint-disable-line no-unused-vars
@@ -11,7 +11,7 @@ function create(token, workspaceId, projectId) { // eslint-disable-line no-unuse
 
 /**
  * Workspace内のProjectを取得する
- * @param {Integer} workspaceId 【任意】ワークスペースID
+ * @param {?number} workspaceId 【任意】ワークスペースID
  * @return {Object} Projectのオブジェクト
  */
 function getProjectsInWorkspace(workspaceId) { // eslint-disable-line no-unused-vars
@@ -20,7 +20,7 @@ function getProjectsInWorkspace(workspaceId) { // eslint-disable-line no-unused-
 
 /**
  * Projectの情報を取得する
- * @param {Integer} projectId 【任意】プロジェクトID
+ * @param {?number} projectId 【任意】プロジェクトID
  * @return {Object} Projectのオブジェクト
  */
 function getSpecificProject(projectId) { // eslint-disable-line no-unused-vars
@@ -29,7 +29,7 @@ function getSpecificProject(projectId) { // eslint-disable-line no-unused-vars
 
 /**
  * Project内のTaskを取得する
- * @param {Integer} projectId 【任意】プロジェクトID
+ * @param {?number} projectId 【任意】プロジェクトID
  * @return {Object} Taskのオブジェクト
  */
 function getTasksInProject(projectId) { // eslint-disable-line no-unused-vars
@@ -38,7 +38,7 @@ function getTasksInProject(projectId) { // eslint-disable-line no-unused-vars
 
 /**
  * Taskの情報を取得する
- * @param {Integer} taskId 【必須】タスクID
+ * @param {number} taskId 【必須】タスクID
  * @return {Object} Taskのオブジェクト
  */
 function getSpecificTask(taskId) { // eslint-disable-line no-unused-vars
@@ -48,10 +48,21 @@ function getSpecificTask(taskId) { // eslint-disable-line no-unused-vars
 /**
  * Taskを検索する
  * https://asana.com/developers/documentation/getting-started/search-api
- * @param {Integer} workspaceId 【任意】ワークスペースID
- * @param {Object} params 【任意】パラメーター
+ * @param {?number} workspaceId 【任意】ワークスペースID
+ * @param {Object} params 【必須】パラメーター
  * @return {Object} Taskのオブジェクト
  */
 function searchTask(workspaceId, params) { // eslint-disable-line no-unused-vars
+  throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
+}
+
+/**
+ * Project内のTaskをカウントする
+ * https://asana.com/developers/api-reference/projects#get-task-counts
+ * @param {?number} projectId 【任意】プロジェクトID
+ * @param {Array<string>} fields 【必須】カウントするフィールド
+ * @return {Object} データのオブジェクト
+ */
+function countProjectTasks(projectId, fields) { // eslint-disable-line no-unused-vars
   throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
 }
